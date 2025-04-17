4c60b507338f5c7651fe59d0ce5a867af1ccafbf
Aleksa Dragnic <aleksa_dragnic@yahoo.com>
2025-04-17 11:03:08 +0000

    update4

--- boilerplate-project-timestamp-main/index.js
+++ boilerplate-project-timestamp-main/index.js
@@ -39,7 +39,7 @@ app.get("/api/:date", function (req, res) {
   });
 });
 
-app.get("/api", (req, ress) => {
+app.get("/api", (req, res) => {
   res.json({
     unix: new Date().getTime(),
     utc: new Date().toUTCString()
